88bc66d48732693dca6b9ca2c98d265e79d8b323
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-08 14:09:43 +0000

    Added constants at script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Set Up Constants with Query Selector
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
+
+// Apply Event Listeners
+about.addEventListener('click', function(e) {
+    
+});
